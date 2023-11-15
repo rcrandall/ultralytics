16854aa8c8cd9fb85ddbd310763d6cba8ef7c66f
Robert Crandall <rcrandal@amazon.com>
2023-11-15 22:51:25 +0000

    Remove device_ids argument from dist.barrier calls

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -33,10 +33,10 @@ def torch_distributed_zero_first(local_rank: int):
     """Decorator to make all processes in distributed training wait for each local_master to do something."""
     initialized = torch.distributed.is_available() and torch.distributed.is_initialized()
     if initialized and local_rank not in (-1, 0):
-        dist.barrier(device_ids=[local_rank])
+        dist.barrier() #device_ids=[local_rank])
     yield
     if initialized and local_rank == 0:
-        dist.barrier(device_ids=[0])
+        dist.barrier() #device_ids=[0])
 
 
 def smart_inference_mode():
